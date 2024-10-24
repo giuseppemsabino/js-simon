169d6e6a7ad72ea78d4bcb1d5bb697953667089a
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-10-24 17:49:46 +0200

    const declare

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const countdownElement = document.querySelector('.countDown');
+        const randomNumberElements = [
+          document.getElementById('randomNumber1'),
+          document.getElementById('randomNumber2'),
+          document.getElementById('randomNumber3'),
+          document.getElementById('randomNumber4'),
+          document.getElementById('randomNumber5')
+        ];
+        const inputElements = [
+          document.getElementById('input1'),
+          document.getElementById('input2'),
+          document.getElementById('input3'),
+          document.getElementById('input4'),
+          document.getElementById('input5')
+        ];
+        const goButton = document.getElementById('goButton');
+        const correctNumberElement = document.getElementById('correctNumber');
+        
+        let randomNumbers = [];
+        let timer = 30;
+      
+      });
+      
